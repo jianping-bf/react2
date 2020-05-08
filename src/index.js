adeb8eed6b2c1eb2dd14cbea3d6b2d57ebfa1e3d
unknown <jianping.zhou@insightinvestment.com>
2020-05-08 16:54:48 +0100

    no real change

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </>
   );
 }
 
